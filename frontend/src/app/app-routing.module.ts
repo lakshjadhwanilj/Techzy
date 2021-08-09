4d8cf9134e0294a071caadf0bfd54c3440b34a05
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2021-08-09 18:34:12 +0530

    Added new font, about component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+ 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', component: HomeComponent }
 ];
 
